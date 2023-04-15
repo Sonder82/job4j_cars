ef7488cb2814803dec6c1f2ac45be82df37ac0fd
Sonder82 <Himrs82@gmail.com>
2023-04-15 19:30:16 +0500

    1. ToMany [#301848]

--- src/main/java/ru/job4j/cars/model/PriceHistory.java
+++ src/main/java/ru/job4j/cars/model/PriceHistory.java
@@ -6,20 +6,22 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-@Table(name = "auto_user")
+@Table(name = "price_history")
 
-public class User {
+public class PriceHistory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private int id;
-    private String login;
-    private String password;
+    private long before;
+    private long after;
+    private LocalDateTime created;
 }
